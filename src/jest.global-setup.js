2a553310aba1187314c301bf1bafeb33db1a58a0
Tim Sprowl <you@example.com>
2021-10-21 11:48:00 -0400

    use routes arg

--- src/jest.global-setup.js
+++ src/jest.global-setup.js
@@ -3,7 +3,7 @@ import { server } from "@tim-code/server"
 export default () =>
   new Promise((resolve) => {
     global.server = server({
-      dir: process.env.ROUTES_DIR,
+      routes: process.env.ROUTES_DIR,
       port: 0,
       done: () => {
         process.env.PORT = global.server.address().port
