980f3562f98dff224ef034390924815a0d8246cc
Tim Sprowl <you@example.com>
2021-10-21 11:23:15 -0400

    fix paths

--- src/jest.config.js
+++ src/jest.config.js
@@ -5,7 +5,7 @@ const projectDir = dirname(fileURLToPath(import.meta.url))
 
 export default {
   transform: {},
-  globalSetup: `${projectDir}/lib/jest.global-setup.js`,
-  globalTeardown: `${projectDir}/lib/jest.global-teardown.js`,
-  setupFilesAfterEnv: [`${projectDir}/lib/jest.setup.js`],
+  globalSetup: `${projectDir}/jest.global-setup.js`,
+  globalTeardown: `${projectDir}/jest.global-teardown.js`,
+  setupFilesAfterEnv: [`${projectDir}/jest.setup.js`],
 }
