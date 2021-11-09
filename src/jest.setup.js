9af17a6cef816f68df76d7235595efb6788db96f
Tim Sprowl <you@example.com>
2021-11-09 12:01:06 -0500

    remove usage of undocumented environment variables

--- src/jest.setup.js
+++ src/jest.setup.js
@@ -1,11 +1,10 @@
 import browser from "browser-env"
 import fetch from "node-fetch"
 
-const protocol = process.env.PROTOCOL || "http"
-const hostname = process.env.HOSTNAME || "localhost"
-const port = process.env.PORT
+// const protocol = process.env.PROTOCOL || "http"
+// const hostname = process.env.HOSTNAME || "localhost"
+// const port = process.env.PORT
 
-browser({
-  url: `${protocol}://${hostname}:${port}`,
-})
+// unclear what url is used for but appears to need to be set to something
+browser({ url: `http://localhost` })
 global.fetch = fetch
