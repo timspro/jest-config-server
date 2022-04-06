b3f1b46a1f00000e36633a62619c9dab72e4baf7
Tim Sprowl <tim@email>
2022-04-06 11:15:03 -0400

    add ability to specify additional server options

--- src/jest.global-setup.js
+++ src/jest.global-setup.js
@@ -5,6 +5,7 @@ export default () =>
     global.server = server({
       routes: process.env.ROUTES_DIR,
       port: process.env.PORT || 0,
+      ...JSON.parse(process.env.SERVER_OPTIONS || "{}"),
       done: () => {
         process.env.PORT = global.server.address().port
         resolve()
